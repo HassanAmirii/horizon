4cb5cd1eea8022a8322a6f524748a15475e1911f
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-09-01 15:22:32 +0100

    started initial model for user profile

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  isAdmin: false,
+  type: Boolean,
+});
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./middleware/app");
 const dbURI = process.env.MONGODB_URI;
+const jwt = require("jsonwebtoken");
+const task = require("./models/tasks");
 
 mongoose
   .connect(dbURI)
   .then(() => {
     console.log("succesfully connected to mongodb");
-
     const port = process.env.PORT || 3000;
 
     app.listen(port, () => {
