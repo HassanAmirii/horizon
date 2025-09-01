5be5cfc7105249021cbeb721b9a34fea92e0744d
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-09-01 13:58:01 +0100

    intial app set up

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv");
+const mongoose = require("mongoose");
+const app = require("./middleware/app");
+const dbURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("succesfully connected to mongodb");
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+      console.log(`horizon is listening on localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("database cconnection error", err);
+    process.exit(1);
+  });
