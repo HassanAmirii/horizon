2790da1eb5df709ca84445d38eefdc113c06a76d
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-09-02 18:11:41 +0100

    feat; implemented login + jwt to sign user token

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dotenv");
+require("dotenv").config();
+const bcrpt = require("bcrypt");
 const mongoose = require("mongoose");
 const app = require("./middleware/app");
 const dbURI = process.env.MONGODB_URI;
@@ -13,22 +14,47 @@ mongoose
     const port = process.env.PORT || 3000;
 
     app.post("/register", async (req, res) => {
-      const { username, email, passoword } = req.body;
-
       try {
-        const newUser = new User(username, email, passoword);
+        const { username, email, password, isAdmin } = req.body;
+        const newUser = new User({ username, email, password, isAdmin });
+        await newUser.save();
         res.status(201).json({
           message: "you have been succesfully registered",
           user: newUser,
         });
-        await newUser.save();
       } catch (error) {
-        res.status(404).json({
+        res.status(500).json({
           message: error.message,
         });
       }
     });
 
+    app.post("/login", async (req, res) => {
+      const { username, password } = req.body;
+      const user = await User.find(req.body.username);
+      if (!user && !(await bcrpt.compare(User.password, password))) {
+        return res.status(401).json({ error: "invalid credentials" });
+      }
+
+      try {
+        res.status(200).json({
+          message: `you have  succesfully login ${username}`,
+        });
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+
+      const payload = {
+        id: user._id,
+        username: user.username,
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1hr",
+      });
+    });
     app.listen(port, () => {
       console.log(`horizon is listening on localhost:${port}`);
     });
